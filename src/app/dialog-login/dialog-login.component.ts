54bb4fee860e21a50d472d785d369199d7138811
nil <n.spiller@gocaspi.de>
2022-08-22 11:19:28 +0200

    feature/On4T-96_FrontendForApp - added login form

--- src/app/dialog-login/dialog-login.component.ts
+++ src/app/dialog-login/dialog-login.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import axios from "axios";
 
 @Component({
   selector: 'app-dialog-login',
   templateUrl: './dialog-login.component.html',
   styleUrls: ['./dialog-login.component.scss']
 })
+
+
+
 export class DialogLoginComponent implements OnInit {
 public username : string
   public password: string
@@ -17,5 +21,21 @@ public username : string
   }
 submit(){
 console.log("username:",this.username,"password:",this.password)
+  this.getLoggedinUser()
+ // this.getSomeUser()
+}
+
+getLoggedinUser(){
+ let UserPayload = {username: this.username, password: this.password}
+  axios.post("http://localhost:9090/login",JSON.stringify(UserPayload))
+    .then(response => console.log(response.data))
+    .catch(error => console.log(error))
+}
+
+
+getSomeUser(){
+  axios.get("http://localhost:9090/62ff87026b75ab5717808dee")
+    .then(response => console.log(response.data))
+    .catch(error => console.log(error))
 }
 }
